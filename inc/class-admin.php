291407900f599016f00d124b498705cca350c3d8
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-05-27 10:42:08 +0300

    refactor: use `get_post_field()`

--- inc/class-admin.php
+++ inc/class-admin.php
@@ -43,7 +43,7 @@ final class Admin {
 	public function term_meta_box_callback( WP_Post $post ): void {
 		$params = [
 			'id'          => $post->ID,
-			'description' => $post->post_content,
+			'description' => get_post_field( 'post_content', $post->ID, 'edit' ),
 		];
 
 		self::render( 'term-metabox', $params );
